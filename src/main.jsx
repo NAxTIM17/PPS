ce0fc97226057f44e7102575e1f3146f6699d33a
NAxTIM17 <naxtim14@gmail.com>
2024-07-01 16:52:54 -0300

    sergio we lov u.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'rsuite/dist/rsuite.min.css';
+import { CustomProvider } from 'rsuite';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CustomProvider>
+      <App />
+    </CustomProvider>
   </React.StrictMode>,
 )
