b9b5954b231d97d688a55e67a5a973f0aa4a28d4
Mateo González <minidrw@gmail.com>
2024-07-22 19:11:11 -0300

    ref: change some names

--- frontend/src/components/BusinessRouter/index.jsx
+++ frontend/src/components/BusinessRouter/index.jsx
@@ -4,17 +4,17 @@ import ErrorPage from '../../pages/Error';
 const BusinessRouter = () => {
     return (
         <Routes>
-            <Route path='dashboard' element={<div>where I should look to upload or visualize dashboards</div>}>
-                <Route path=':dashboardId' element={<div>where one dashboard should appear</div>}/>
-                <Route path='carga' element={<div>where I should upload data to fill dashboards</div>}/>
+            <Route path='dashboard' element={<div>where to upload or visualize dashboards</div>}>
+                <Route path=':dashboardId' element={<div>each dashboard should appear</div>}/>
+                <Route path='carga' element={<div>where u upload data to fill dashboards</div>}/>
             </Route>
 
             <Route path='cuenta'>
-                <Route path='login' element={<div>log in</div>}/>
-                <Route path='recuperar' element={<div>recover password</div>}/>
+                <Route path='login' element={<div>log in page</div>}/>
+                <Route path='recuperar' element={<div>recover password page</div>}/>
             </Route>
             <Route path='/' element={<Navigate to='dashboard'/>}/>
-            <Route path='*' element={<ErrorPage />}/>
+            <Route path='*' element={<div>404 page</div>}/>
         </Routes>
     )
 }
