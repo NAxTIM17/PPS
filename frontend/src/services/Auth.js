498876ff139a644a252f00eddd93a5f943db879e
Sergio Yalux <randaledomman1@gmail.com>
2024-07-26 00:47:06 -0300

    feat(frontend): create custom axiosInstace & implement auth services

---
+++ frontend/src/services/Auth.js
@@ -0,0 +1,36 @@
+import axios from './axios';
+
+/**
+    * @typedef {Object} AuthResponse
+    * @property {string} token
+*/
+
+/**
+    * Execute a login request
+    * @param {Object} payload
+    * @param {string} payload.email
+    * @param {string} payload.password
+    * @returns {AuthResponse}
+*/
+async function login(payload) {
+    const res = await axios.post('/auth/login', payload, {});
+    return res.data;
+}
+
+/**
+    * Execute a login request
+    * @param {Object} payload
+    * @param {string} payload.username
+    * @param {string} payload.email
+    * @param {string} payload.password
+    * @returns {Promise<AuthResponse>}
+*/
+async function register(payload) {
+    const res = await axios.post('/auth/register', payload, {});
+    return res.data;
+}
+
+export const AuthService = {
+    login,
+    register,
+};
