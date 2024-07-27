08bf9d61550150fec0d052a2473f98dbc8bb4fda
Mateo González <minidrw@gmail.com>
2024-07-27 17:15:10 -0300

    feat(frontend): add users service

---
+++ frontend/src/services/Users.js
@@ -0,0 +1,49 @@
+import axios from './axios';
+
+/**
+    * @typedef {Object} UserResponse
+    * @property {number} _id
+    * @property {string} email
+    * @property {string} name
+    * @property {string} createdAt
+    * @property {string} updatedAt
+*/
+/**
+    * @typedef {Object} DeleteResponse
+    * @property {string} message
+*/
+
+/**
+    * @returns {Promise<UserResponse>}
+*/
+async function getUser() {
+    const res = await axios.get('/users');
+    return res.data;
+} 
+
+/**
+    * Execute a login request
+    * @param {Object} payload
+    * @param {string} payload.name
+    * @param {string} payload.email
+    * @param {string} payload.password
+    * @returns {Promise<UserResponse>}
+*/
+async function updateUser(payload) {
+    const res = await axios.put('/users', payload);
+    return res.data;
+}
+
+/**
+    * @returns {Promise<DeleteResponse>}
+*/
+async function deleteUser() {
+    const res = await axios.delete('/users');
+    return res.data;
+}
+
+export const UsersService = {
+    getUser,
+    updateUser,
+    deleteUser,
+};
