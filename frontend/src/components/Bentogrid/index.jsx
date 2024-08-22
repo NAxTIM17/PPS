3cbdfbaa4a0074392e5477ff91ff9028d0760456
Sergio Yalux <randaledomman1@gmail.com>
2024-08-22 09:59:02 -0300

    feat(front): create header slot in MainContent layout

--- frontend/src/components/Bentogrid/index.jsx
+++ frontend/src/components/Bentogrid/index.jsx
@@ -1,9 +1,9 @@
-const Bentogrid = ({children}) => {
-    return (
-        <div className="grid grid-rows-8 grid-cols-2 w-full h-full gap-7">
-            {children}
-        </div>
-    );
-}
+const Bentogrid = ({ children }) => {
+	return (
+		<div className="grid grid-rows-8 grid-cols-2 w-full h-full gap-brand-2">
+			{children}
+		</div>
+	);
+};
 
 export default Bentogrid;
