7078de0bf1b46d366b38997b27d5df5ec65855db
Sergio Yalux <randaledomman1@gmail.com>
2024-11-20 20:21:27 -0300

    feat(front): show createdAt as history item title

--- frontend/src/components/Historial/elements/HistoryItem.jsx
+++ frontend/src/components/Historial/elements/HistoryItem.jsx
@@ -2,33 +2,19 @@ import { IconExternalLink } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../../router/config';
 
-export default function HistoryItem({ date, drugstore, totalPrice }) {
+export default function HistoryItem({ date, drugstore }) {
 	const navigate = useNavigate();
-	console.log(drugstore[0]?.oferta_valida?.inicio);
 
 	const handledata = () => {
-		const json = drugstore[0];
-		const data = [json];
-		localStorage.setItem('pharmacyData', JSON.stringify(data));
+		localStorage.setItem('pharmacyData', JSON.stringify([...drugstore]));
 		navigate(ROUTES.AUTHED_ROUTES.NEW_DASHBOARD);
 	};
 
 	return (
 		<div className="w-full h-14 rounded-inner-border p-spacing flex flex-row items-center justify-center gap-16 bg-color-bg-surface">
-			<h1 className="text-2xl font-bold grow">
-				{drugstore[0]?.drogueria}
+			<h1 className="text-xl grow">
+				{new Date(date).toLocaleDateString()}
 			</h1>
-			<div className="flex gap-spacing "></div>
-			{/* <h1 className="text-2xl font-bold ">
-				{new Intl.NumberFormat({
-					style: 'currency',
-					currency: 'USD',
-				}).format(totalPrice)}
-			</h1> */}
-			<div className="flex font-semibold">
-				<h1>{drugstore[0]?.oferta_valida?.inicio}-</h1>
-				<h1>{drugstore[0]?.oferta_valida?.fin}</h1>
-			</div>
 			<IconExternalLink
 				size={20}
 				className="cursor-pointer"
--- frontend/src/components/Historial/index.jsx
+++ frontend/src/components/Historial/index.jsx
@@ -1,35 +1,20 @@
-import { DateRangePicker, Pagination, Dropdown, Button } from 'rsuite';
-import { IconX, IconSearch, IconListSearch } from '@tabler/icons-react';
-import { useState } from 'react';
+import { IconListSearch } from '@tabler/icons-react';
 
 import HistoryItem from './elements/HistoryItem';
 
-const FILTERS = ['Drogueria', 'Productos', 'Precio', 'Fecha'];
-
 const History = ({ arrayDates }) => {
-	const [activePage, setActivePage] = useState(1);
-	const [arrayFilter, setArrayFilter] = useState([]);
-	// const [arrayDates, setArrayDates] = useState([{}, {}]);
-
-	// for (let i = 0; i < arrayDates.length; i++) {
-	// 	console.log(arrayDates[i]?.history[0]?.drogueria)
-	// }
-
 	return (
 		<div className="w-full h-full flex flex-col p-spacing overflow-auto overflow-x-hidden">
 			<div className="w-full flex">
 				{arrayDates.length > 0 ? (
 					<div className="overflow-auto flex flex-col gap-spacing w-full">
-						{arrayDates.map((item, index) => {
-							const { history } = item;
-							return (
-								<HistoryItem
-									key={index}
-									date={item.dateFrom}
-									drugstore={history}
-								/>
-							);
-						})}
+						{arrayDates.map((item, index) => (
+							<HistoryItem
+								key={index}
+								date={item.createdAt}
+								drugstore={item.history}
+							/>
+						))}
 					</div>
 				) : (
 					<div className="w-full h-full flex justify-center items-center text-color-icons">
